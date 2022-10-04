1839de38c9555c08c35022701a5ede5aee59f911
phryszlo <phryszlo@gmail.com>
2022-10-04 10:00:15 -0400

    authServer works with refresh token

---
+++ server/authServer.js
@@ -0,0 +1,52 @@
+
+require('dotenv').config()
+
+const express = require('express');
+const app = new express();
+const jwt = require('jsonwebtoken');
+
+const PORT = 4040;
+
+app.use(express.json())
+
+app.post('/login', (req, res) => {
+  const username = req.body.username
+  const user = { name: username }
+
+  const accessToken = generateAccessToken(user)
+  const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET)
+  refreshTokens.push(refreshToken)
+  res.json({ accessToken: accessToken, refreshToken: refreshToken })
+})
+
+// don't do this. use redis or something.
+let refreshTokens = []
+
+app.post('/token', (req, res) => {
+  const refreshToken = req.body.token
+  if (refreshToken == null) return res.sendStatus(401)
+  if (!refreshTokens.includes(refreshToken)) return res.sendStatus(403)
+  jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
+    if (err) return res.sendStatus(403)
+    const accessToken = generateAccessToken({ name: user.name })
+    res.json({accessToken: accessToken})
+  })
+})
+
+function generateAccessToken(user) {
+  return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '25s' })
+}
+
+// function authenticateToken(req, res, next) {
+//   const authHeader = req.headers['authorization']
+//   const token = authHeader && authHeader.split(' ')[1]
+//   if (token == null) return res.sendStatus(401)
+
+//   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+//     if (err) return res.sendStatus(403)
+//     req.user = user
+//     next()
+//   })
+// }
+
+app.listen(PORT, () => console.log(`on ${PORT}`));
